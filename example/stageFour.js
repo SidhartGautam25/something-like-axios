1eb7c2e925fda34ca13733a3c7c321ae820925e2
sidhart25 <navneetnishchal1420@gmail.com>
2025-05-01 19:00:19 +0530

    stage four completed

---
+++ example/stageFour.js
@@ -0,0 +1,31 @@
+const myHttp = require("../lib");
+
+// promise style
+myHttp("https://jsonplaceholder.typicode.com/posts/1")
+  .then((res) => {
+    console.log("Got response via Promise:\n", res);
+  })
+  .catch((err) => {
+    console.error("Error:", err);
+  });
+
+// async await style
+(async () => {
+  try {
+    const res = await myHttp("https://jsonplaceholder.typicode.com/posts/2");
+    console.log("Async/Await Response:", res);
+  } catch (e) {
+    console.error("Async/Await Error:", e);
+  }
+})();
+
+// stream + callback style
+const req = myHttp(
+  "https://jsonplaceholder.typicode.com/posts/3",
+  (err, data) => {
+    console.log("Callback result:", data.length);
+  }
+);
+
+req.on("data", (chunk) => console.log("Chunk size:", chunk.length));
+req.on("end", () => console.log("Stream end"));
